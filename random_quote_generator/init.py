8c0ff5ede1cf0c4d5922a078df19c6ae576194bd
pawasRituja <ritujapawas.djr@gmail.com>
2022-06-21 13:55:36 +0530

    random-quotes

---
+++ random_quote_generator/init.py
@@ -0,0 +1,10 @@
+"""
+Random Quote Generator
+======================
+
+Get random quote from our database of programming wisdom
+"""
+from random_quote_generator import get_quote
+from get_quote import get_quote
+
+__all__ = ["get_quote"]
